584989e0299b67505b4e3062444df69412ee3ebe
Rafael Fernández López <ereslibre@ereslibre.es>
2011-12-30 03:43:16 +0100

    Check if this loads applicationcontroller

--- lib/realtime-validations.rb
+++ lib/realtime-validations.rb
@@ -1,6 +1,13 @@
 module RealtimeValidations
 
   class RealtimeValidations < Rails::Engine
+
+    initializer 'realtime_validations.init' do |app|
+      ActiveSupport.on_load(:action_controller) do
+         include RealtimeValidations::ValidationsController
+      end
+    end
+
   end
 
 end
